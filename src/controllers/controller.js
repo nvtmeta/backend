f97dece22e283f799fa3d32fbfe19f2500eff7f5
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-19 13:13:18 +0700

    video35

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,13 +2,14 @@ const connection = require('../config/database')
 
 const getHomePage = (req, res) => {
 
-    connection.query(
-        'SELECT * FROM Users',
-        function (err, results, fields) {
-            console.log(results); // results contains rows returned by server
-            res.send(JSON.stringify(results))
-        }
-    );
+    return res.render('home.ejs')
+    // connection.query(
+    //     'SELECT * FROM Users',
+    //     function (err, results, fields) {
+    //         console.log(results); // results contains rows returned by server
+    //         res.send(JSON.stringify(results))
+    //     }
+    // );
 }
 
 // simple query
