d38115b4218cfc6ab143a0ba3a25196a00b377e3
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-19 09:50:40 +0700

    video30

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const webRouter = require('./routes/web')
 const app = express()
 const port = process.env.PORT || 8081;
 const hostname = process.env.HOST_NAME
+const mysql = require('mysql2');
+
 
 
 //config
@@ -16,6 +18,23 @@ configViewEngine(app)
 app.use('/', webRouter)
 
 
+//test connection
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, //default 3306
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+});
+
+// simple query
+connection.query(
+    'SELECT * FROM Users',
+    function (err, results, fields) {
+        console.log(results); // results contains rows returned by server
+    }
+);
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
