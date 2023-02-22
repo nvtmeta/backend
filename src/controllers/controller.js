893f13f808b4cd0e84f49468c329fe8cec84980d
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-22 18:02:54 +0700

    video44

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,15 +1,12 @@
 const connection = require('../config/database')
-const { getAllUser } = require('../services/CRUDService')
+const { getAllUser, PostNewUser, getUserById } = require('../services/CRUDService')
 const getHomePage = async (req, res) => {
     let result = await getAllUser()
     return res.render('home.ejs', { listUsers: result })
 }
 
 const postCreateUser = async (req, res) => {
-    let { email, name, city } = req.body
-    let [result, fields] = await connection.query(`  INSERT INTO Users (email, name, city)
-    VALUES (?, ?, ? )`,
-        [name, email, city],)
+    let results = await PostNewUser(req)
     res.send('create new user success')
 }
 
@@ -17,9 +14,9 @@ const postCreateUser = async (req, res) => {
 const getCreatePage = (req, res) => {
     return res.render('create.ejs')
 }
-const putUpdateUser = (req, res) => {
-    console.log(req.params)
-    return res.render('update.ejs')
+const putUpdateUser = async (req, res) => {
+    let user = await getUserById(req)
+    return res.render('update.ejs', { user })
 }
 
 module.exports = { getHomePage, postCreateUser, getCreatePage, putUpdateUser }
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -5,6 +5,26 @@ const getAllUser = async () => {
     return result
 
 }
+
+const PostNewUser = async (req, res) => {
+    let { email, name, city } = req.body
+    let [result, fields] = await connection.query(`  INSERT INTO Users (email, name, city)
+    VALUES (?, ?, ? )`,
+        [name, email, city],)
+
+    return result
+}
+
+const getUserById = async (req) => {
+    const userId = req.params.id
+    let [results, fields] =
+        await connection.query(`select * from Users where id = ?`, [userId])
+    let user = results && results.length > 0 ? results[0] : ''
+    return user
+}
+
+
+
 module.exports = {
-    getAllUser
+    getAllUser, PostNewUser, getUserById
 }
